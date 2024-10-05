ab32aeb46a5598d165ebd4bcbcdcdaabdfa095e0
Andrew Stamm <andrewstamm@me.com>
2024-10-05 15:24:25 -0500

    Initial Commit

---
+++ Zap Comics/Zap_ComicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zap_ComicsApp.swift
+//  Zap Comics
+//
+//  Created by Andrew Stamm on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Zap_ComicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
